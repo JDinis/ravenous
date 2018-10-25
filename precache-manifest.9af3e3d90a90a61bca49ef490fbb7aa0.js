a774dd224447a667a8c26a5c422caaca2786c42b
JDinis <j.p.dinis89@gmail.com>
2018-10-25 10:42:56 +0100

    Updates

---
+++ precache-manifest.9af3e3d90a90a61bca49ef490fbb7aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "c0990d404b7d9ff5f6fe",
+    "url": "/ravenous/static/js/main.c0990d40.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/ravenous/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "c0990d404b7d9ff5f6fe",
+    "url": "/ravenous/static/css/main.71dabf92.chunk.css"
+  },
+  {
+    "revision": "a56bac7ec3287ce70bdefaafdfdf47ef",
+    "url": "/ravenous/index.html"
+  }
+];
